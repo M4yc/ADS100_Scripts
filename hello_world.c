2a5ee73b8dcea4c064997c4b6f9573a570bb9d51
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-03-13 10:56:17 -0300

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,7 +1,32 @@
-#include<stdio.h>
-#include<conio.h>
+#include <stdio.h>
 
-int main(){
-    printf("Hello, world");
+#ifdef _WIN32
+#include <conio.h> // Biblioteca específica do Windows para getch()
+#else
+#include <stdio_ext.h> // Biblioteca específica do Linux para limpar o buffer do teclado
+#endif
+
+int main() {
+    int num1, num2, resultado;
+
+    printf("Digite o primeiro valor: ");
+    scanf("%d", &num1);
+
+    printf("Digite o segundo valor: ");
+    scanf("%d", &num2);
+
+    resultado = num1 + num2;
+
+    printf("A soma dos dois valores é: %d\n", resultado);
+
+#ifdef _WIN32
+    printf("Pressione qualquer tecla para sair...");
+    getch(); // Espera o usuário pressionar uma tecla no Windows
+#else
+    printf("Pressione Enter para sair...");
+    __fpurge(stdin); // Limpa o buffer do teclado no Linux
+    getchar(); // Espera o usuário pressionar Enter no Linux
+#endif
+
     return 0;
 }
