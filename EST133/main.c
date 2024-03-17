25ba4723068167da125d72ad1d86ef65f06747ba
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-03-17 17:31:08 -0300

    compile windows

--- EST133/main.c
+++ EST133/main.c
@@ -5,9 +5,14 @@ int soma(int a, int b);
 int main(){
     int x = 5;
     int y = 3;
+    int resultado;
 
     resultado = soma(x, y);
 
     printf("A soma de %d e %d é %d\n", x, y, resultado);
     return 0;
+}
+
+int soma(int a, int b){
+    return a + b;
 }
