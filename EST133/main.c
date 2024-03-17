b58ea4333dce0a09b9888e25240df329156b444a
Maycon Araujo <mayconvbatista84@gmail.com>
2024-03-17 17:26:55 -0300

    t

---
+++ EST133/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int soma(int a, int b);
+
+int main(){
+    int x = 5;
+    int y = 3;
+
+    resultado = soma(x, y);
+
+    printf("A soma de %d e %d é %d\n", x, y, resultado);
+    return 0;
+}
