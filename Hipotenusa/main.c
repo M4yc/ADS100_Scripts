c5b778848676502409b781b332792e423ac10e97
Maycon Araujo <mayconvbatista84@gmail.com>
2024-03-19 22:57:48 -0300

    Create main.c

---
+++ Hipotenusa/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    float a,b,c;
+
+    printf("Informe o valor de A: ");
+    scanf("%f",&a);
+
+    printf("Informe o valor de B: ");
+    scanf("%f",&b);
+
+    c = sqrt((a*a)+(b*b));
+
+    printf("A hipotenusa é: %f", c);
+
+    return 0;
+}
