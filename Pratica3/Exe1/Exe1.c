72a66537a9c4f1e5922b4903467393cf7c167178
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-04-27 12:15:32 -0300

    :sparkles: Exe1 -Pratica3

--- Pratica3/Exe1/Exe1.c
+++ Pratica3/Exe1/Exe1.c
@@ -2,54 +2,49 @@
 #include <math.h>
 #include <locale.h>
 
-
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
     int opmenu;
-    float resultado, n1, n2;
+    double resultado, n1, n2;
     
     printf("Digite o numero para fazer as respectivas operações\n1 - Adição\n2 - Subtração\n3 - Multiplicação\n4 - Divisão\n5 - Potencialização\n6 - Raiz Quadrada\n");
     
     scanf("%d",&opmenu);
 
     printf("Informe o Primeiro Valor: ");
-    scanf("%f",&n1);
+    scanf("%lf",&n1);
     printf("Informe o Segundo Valor: ");
-    scanf("%f",&n2);
+    scanf("%lf",&n2);
     
     switch (opmenu){
     case 1:
         resultado = n1 + n2;
-        printf("A soma dos numeros digitados é : %.2f\n",resultado);
+        printf("A soma dos números digitados é : %.2lf\n",resultado);
         break;
     case 2:
-        
         resultado = n1 - n2;
-        printf("A soma dos numeros digitados é : %.2f\n",resultado);
+        printf("A subtração dos números digitados é : %.2lf\n",resultado);
         break;
     case 3:
-        
         resultado = n1 * n2;
-        printf("A Multiplicação dos numeros digitados é : %.2f\n",resultado);
+        printf("A multiplicação dos números digitados é : %.2lf\n",resultado);
         break;
     case 4:
-        
-        resultado = n1/n2;
-        printf("A Divisão dos numeros digitados é : %.2f\n",resultado);
+        resultado = n1 / n2;
+        printf("A divisão dos números digitados é : %.2lf\n",resultado);
         break;
     case 5:
-        
-        resultado = pow(n1, n2);
-        printf("A Potencialição dos numeros digitados é : %.2f\n",resultado);
+        resultado = pow(6, 8);
+        printf("A potenciação dos números digitados é : %.2lf\n",resultado);
         break;
     case 6:
-        
-        resultado = sqrt(n1);
-        printf("A raiz quadrada dos numeros digitados é : %.2f\n",resultado);
+        resultado = sqrt(9); // Raiz quadrada dos números digitados
+        printf("A raiz quadrada dos números digitados é : %.2lf\n",resultado);
         break;
     default:
-    printf("Valor Invalido.");
+        printf("Valor Inválido.\n");
         break;
     }
+    return 0;
 }
