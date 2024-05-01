d919436e21d27db82264e044de5b7402521ff361
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-05-01 15:11:55 -0300

    :recycle: Att Exe6-7 List2

--- Lista_exe2/Exe6/Exe6.c
+++ Lista_exe2/Exe6/Exe6.c
@@ -16,16 +16,16 @@ int main(){
     
     if (produto >= 1 && produto <= 10){
         valor = 10 * quantidade;
-        printf("Produto 1 a 10 Valor de R$ 10,00\n");
+        printf("Produtos 1 a 10 Valor de: R$ 10,00\n");
     } else if (produto >= 11 && produto <= 20){
         valor = 15 * quantidade;
-        printf("Produto 11 a 20 Valor de R$ 15,00\n");
+        printf("Produtos 11 a 20 Valor de: R$ 15,00\n");
     } else if (produto >= 21 && produto <= 30){
         valor = 20 * quantidade;
-        printf("Produto 21 a 30 Valor de R$ 20,00\n");
+        printf("Produtos 21 a 30 Valor de: R$ 20,00\n");
     } else if (produto >= 31 && produto <= 40){
         valor = 30 * quantidade;
-        printf("Produto 31 a 40 Valor de R$ 30,00\n");
+        printf("Produtos 31 a 40 Valor de: R$ 30,00\n");
     }
 
     if (valor < 250){
@@ -42,8 +42,6 @@ int main(){
         printf("Valor do desconto de 15%%: R$ %.2f\n", valor*0.15);
         printf("Valor Total com desconto: R$ %.2f\n", valor-(valor*0.15));
     }
-    
-    
     
     return 0;
 }
--- Lista_exe2/Exe7/Exe7.c
+++ Lista_exe2/Exe7/Exe7.c
@@ -1,65 +1,44 @@
 #include <stdio.h>
-#include <math.h>
 #include <locale.h>
 
-int main(){
+int main() {
     setlocale(LC_ALL, "Portuguese");
 
     int produto;
 
-    printf("Informe o codigo do produto: ");
+    printf("Informe o código do produto: ");
     scanf("%d", &produto);
 
-    switch (produto)
-    {
-    case 1:
-        printf("Código %d Alimento não-perecível.", produto);
-        break;
-    case 2:
-        printf("Código %d Alimento perecível.", produto);
-        break;
-    case 3:
-        printf("Código %d Alimento perecível.", produto);
-        break;
-    case 4:
-        printf("Código %d Alimento perecível.", produto);
-        break;
-    case 5:
-        printf("Código %d Vestuário.", produto);
-        break;
-    case 6:
-        printf("Código %d Vestuário.", produto);
-        break;
-    case 7:
-        printf("Código %d Higiene pessoal.", produto);
-        break;
-    case 8:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 9:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 10:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 11:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 12:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 13:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 14:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    case 15:
-        printf("Código %d Limpeza e utensílios domésticos.", produto);
-        break;
-    default:
-        printf("Código %d Inválido", produto);
-        break;
+    switch (produto) {
+        case 1:
+            printf("Código %d: Alimento não-perecível.\n", produto);
+            break;
+        case 2:
+        case 3:
+        case 4:
+            printf("Código %d: Alimento perecível.\n", produto);
+            break;
+        case 5:
+        case 6:
+            printf("Código %d: Vestuário.\n", produto);
+            break;
+        case 7:
+            printf("Código %d: Higiene pessoal.\n", produto);
+            break;
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+            printf("Código %d: Limpeza e utensílios domésticos.\n", produto);
+            break;
+        default:
+            printf("Código %d: Inválido.\n", produto);
+            break;
     }
+
     return 0;
 }
