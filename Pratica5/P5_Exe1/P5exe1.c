52c48f82587ca2085b2b6cf9e7d9f16b43d7ee1d
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-05-07 22:40:55 -0300

    :recycle: Update P5Exe1

--- Pratica5/P5_Exe1/P5exe1.c
+++ Pratica5/P5_Exe1/P5exe1.c
@@ -13,6 +13,16 @@ int main(){
 
     while(-1){
         
+        printf("Menu de Opções Food & Code:\n");
+        printf("[ 1 ] Pizza.\n");
+        printf("[ 2 ] Hamburguer.\n");
+        printf("[ 3 ] Macarrão.\n");
+        printf("[ 4 ] Batata Frita.\n");
+        printf("[ 5 ] Onion Rings.\n");
+        printf("[ 6 ] Refrigerante.\n");
+        printf("[ 7 ] Cerveja.\n");
+        printf("[ 0 ] Para finalizar o pedido.\n");
+
         printf("Informe o numero do pedido: ");
         scanf("%d",&num_ped);
         if (num_ped == 0){
