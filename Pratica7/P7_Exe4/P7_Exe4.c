35cc6acd57c599b153644e809b9df6d6829de20f
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-05-27 22:52:23 -0300

    :recycle: Update P7_Exe4

--- Pratica7/P7_Exe4/P7_Exe4.c
+++ Pratica7/P7_Exe4/P7_Exe4.c
@@ -14,26 +14,25 @@
 
 int main(){
 
-    int VET1[10] = {3,5,4,2,2,5,3,2,5,9}, VET2[10] = {7,15,20,0,18,4,55,23,8,6}, VET3[20];
-/*
-    for( int x=0; x < 5; x++){
+    int VET1[10] = {3, 5, 4, 2, 2, 5, 3, 2, 5, 9};
+    int VET2[10] = {7, 15, 20, 0, 18, 4, 55, 23, 8, 6};
+    int VET3[20];
+
+    int k = 0, j = 0;
+
+    for( int x=0; x < 20; x++){
         if(x % 2 == 0){
-            VET3[x] = VET1[x];    
+            VET3[x] = VET1[k++];    
         }else{
-            VET3[x+1] = VET2[x];    
+            VET3[x] = VET2[j++];    
         }
         
     }
-*/
 
-    for (size_t i = 0; i < 20; i=i+2){
-        VET3[i] = VET1[i];
-        VET3[i+1] = VET2[i];
-
-        printf("%d ",VET3[i]);
-    }
-
-
+for (int i = 0; i < 20; i++)
+    {
+    printf("%d ",VET3[i]);
+}
 
     system("\npause");
     return 0;
