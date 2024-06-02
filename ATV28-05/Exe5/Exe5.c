f9d117146c4781360bec2a336fb58c387fedba4e
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-06-02 11:29:42 -0300

    :bug:  Remove bug

--- ATV28-05/Exe5/Exe5.c
+++ ATV28-05/Exe5/Exe5.c
@@ -2,16 +2,14 @@
 #include <string.h>
 
 int main(){
-    char str1[100],str2[100];
+    char str1[100], str2[100];
     printf ("Entre com uma string: ");
     fgets(str1, sizeof(str1), stdin);
     printf ("Enter com outra string: ");
-    fgets(str1, sizeof(str1), stdin);
-    if (strcmp(str1, str2)){
-        
+    fgets(str2, sizeof(str2), stdin);
+    if (strcmp(str1, str2) == 0){
         printf ("\n\nAs das strings sao iguais.");
     }else{ 
-        
         printf ("\n\nAs das strings sao diferentes.");
     }
     return(0);
