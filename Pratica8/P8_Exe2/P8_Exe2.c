193a1cf173b1f512b210ab7737d187747353df0e
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-06-04 10:58:24 -0300

    :sparkles: Upadate P8

--- Pratica8/P8_Exe2/P8_Exe2.c
+++ Pratica8/P8_Exe2/P8_Exe2.c
@@ -2,8 +2,8 @@
  * File Name: P8_Exe2.c
  * Developer: Maycon
  * Creation Date: 03/06/2024
- * Description: ??????
- * Last Modification: 03/06/2024
+ * Description: Programa para ordenar um vetor em ordem decrescente
+ * Last Modification: 04/06/2024
  * Modified by: Maycon
 */
 
@@ -14,28 +14,37 @@
 int main(){
 
     int vetor[10];
+    int vetorOrd[10];
     int valor = 0;
 
     for (int x = 0; x < 10; x++){
         printf("Informe o valor de V[%d]: ", x);
         scanf("%d",&vetor[x]);
+        vetorOrd[x] = vetor[x];
     }
     
     for (int i = 0; i < 10-1; i++){
         for (int j = i + 1; j < 10; j++){
             if (vetor[i] < vetor[j]){
                 valor = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = valor;
+                vetorOrd[i] = vetorOrd[j];
+                vetorOrd[j] = valor;
             }
             
         }
     }
-    for (int x = 0; x < 10; x++){
-        printf("%d ", vetor[x]);
+
+    printf("Vetor original:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", vetor[i]);
     }
+    printf("\n");
 
-
-    printf("\n %d ", sizeof(vetor));
+    printf("Vetor ordenado em ordem decrescente:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", vetorOrd[i]);
+    }
+    printf("\n");
+    
     return 0;
 }
--- Pratica8/P8_Exe3/P8_Exe3.c
+++ Pratica8/P8_Exe3/P8_Exe3.c
@@ -1,18 +1,32 @@
 /*
- * File Name: P8_Exe2.c
+ * File Name: P8_Exe3.c
  * Developer: Maycon
  * Creation Date: 03/06/2024
- * Description: ??????
- * Last Modification: 03/06/2024
+ * Description: Programa para receber a entrada de uma frase e contar quantas vogais tem na frase.
+ * Last Modification: 04/06/2024
  * Modified by: Maycon
 */
 
 #include <stdio.h>
 #include <locale.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
 
     char frase[200];
+    int ContVoga = 0;
+
+    printf("Digite uma frase (maximo 199 caracteres): ");
+    fgets(frase, sizeof(frase), stdin);
+
+    for(int i = 0; i < strlen(frase); i++){
+        if (frase[i] == 'A' || frase[i] == 'a' || frase[i] == 'E' || frase[i] == 'e' || frase[i] == 'I' || frase[i] == 'i' || frase[i] == 'O' || frase[i] == 'o' || frase[i] == 'U' || frase[i] == 'u'){
+            ContVoga ++;
+        }
+        
+    }
     
+
+    printf("Na frase digitada contem %d vogais", ContVoga);
 }
