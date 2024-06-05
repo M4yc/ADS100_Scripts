8d80b65aa8ceb07918f25501301ad0a77af48916
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-06-05 10:54:35 -0300

    :sparkles: Update JogoDaVelha add func
    Adicionei funções para reset e formatei a saída mas ainda falta bastante coisa

--- TrabalhoFinal/JogoDaVelha.c
+++ TrabalhoFinal/JogoDaVelha.c
@@ -12,38 +12,70 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/*  Funções do Game */
+void resetGame(char jogo[3][3]);
+
+void printGame(char jogo[3][3]);
+
 int main(){
 
     setlocale(LC_ALL, "Portuguese");
     
     char input;
 
-    char jogo[3][3] = {{'-','-','-'},
-                        {'-','-','-'},
-                        {'-','-','-'}};
+    char jogo[3][3] = {{'.','.','.'},
+                        {'.','.','.'},
+                        {'.','.','.'}};
     
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
             printf("Informe o valor para a posição [%d , %d] (X ou O): ", i, j);
             scanf(" %c",&input);
             input = toupper(input);
-            while (input != 'X' && input != 'O'){
-                printf("Entrada invalida!. Informe o valor para a posição [%d , %d] (X ou O): ", i, j);
-                scanf(" %c",&input);
-                input = toupper(input);
+            while (1){
+                if (input == 'X' || input == 'O'){
+                    break;
+                }
+                else{
+                    printf("Entrada invalida!. Informe o valor para a posição [%d , %d] (X ou O): ", i, j);
+                    scanf(" %c",&input);
+                    input = toupper(input);
+                }
             }
             jogo[i][j] = input;
+
+            while ((getchar()) != '\n'); //Limpar Buffer
         }
         printf("\n");
     }
 
+    printGame(jogo);
+
+    resetGame(jogo);
+
+    printGame(jogo);
+
+    return 0;
+}
+
+void resetGame(char jogo[3][3]){
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            jogo[i][j] = '.';
+        }
+        
+    }
+}
+
+void printGame(char jogo[3][3]){
     printf("Tabuleiro Final: \n");
     for (int i = 0; i < 3; i++){
         for (int j = 0; j < 3; j++){
-            printf(" %c ",jogo[i][j]);
+            printf("| %c ", jogo[i][j]);
         }
+        printf("|");
+        printf("\n");
+        printf("------------");
         printf("\n");
     }
-
-    return 0;
 }
